3c2da12b009f7895d3e64eb53591fc787fde7bb8
mo <mo@openmailbox.org>
2017-01-03 22:54:28 -0500

    changed the way the snake is drawn to the screen, added delay

--- snake.py
+++ snake.py
@@ -1,11 +1,14 @@
 import curses
 import random
+import time
+
 
 def init():
     scr = curses.initscr()
     curses.noecho()
     curses.cbreak()
     scr.keypad(1)
+    scr.nodelay(1)
     curses.curs_set(0)
     return scr
 
@@ -21,59 +24,73 @@ def snake(postions, eat,scr):                   # it makes the snake
     scr.addch(postions[2], postions[3]," ", curses.A_REVERSE)        # spot to be eaten
     scr.refresh 
 
-def checkkey (key,postions):
+def checkkey (key,postions,ea,scr,dire):
     if key == 113:                                                  # q to exit
         postions[4] = 1
-
     elif key == 100:                                                  # moves right
-        postions[1] +=1
-        snake(postions,ea,scr)
-        
+        dire = "right"
     elif key == 119:                                                   # moves up
-        postions[0] -= 1
-        snake(postions,ea,scr)
-
+        dire = "up"
     elif key == 115:                                                  # moves down
-        postions[0] += 1
-        snake(postions,ea,scr)
-
+        dire = "down"
     elif key == 97:                                                   # moves left
-        postions[1] -=1
-        snake(postions,ea,scr)
-
-    return postions 
+        dire = "left"
+    return dire 
     
+def movement(dire):
+    if dire == "right":
+        mm = [1,0]
+    if dire == "left":
+        mm = [-1,0]
+    if dire == "up":
+        mm = [0,-1]
+    if dire == "down":
+        mm = [0,1]
+    return mm    
+
+
 scr = init() 
 
-dim = scr.getmaxyx() 
-maxy = dim[0]
-maxx =  dim[1]
+def main(scr):
+    dim = scr.getmaxyx() 
+    maxy = dim[0]
+    maxx =  dim[1]
 
-#[snake y, snake x, mouse y, mouse x]
-postions = [dim[0]/2, dim[1]/2, random.randint(1,maxy), random.randint(1,maxx), 0]
-
-#screen values for mouse postions.
-
-ea = 1                                                           # start at size 1
-
-snake(postions,ea,scr)                                         # creates the starting snake
-
-
-while 1:
-    key = scr.getch()         
-
-    postions = checkkey(key,postions)
-
-    if postions[4] == 1:
-        break
+    dire = "right"
     
-    if postions[1] == postions[3] and postions[0] == postions[2]:                                    # adds one to the string in snake when you reach the random point
-        ea +=1
-        postions[2] = random.randint(1,maxy)
-        postions[3] = random.randint(1,maxx)
- 
-    if postions[1]  == dim[1]-1 or postions[0] == dim[0]-1 or postions[1] == 1 or postions[0] == 1:             # ends game when you hit a border to avoid crashing 
-        break
+    #[snake y, snake x, mouse y, mouse x] sets starting postions for snake and mouse
+    postions = [dim[0]/2, dim[1]/2, random.randint(1,maxy), random.randint(1,maxx), 0]
 
-term(scr)
+    # start at size 1
+    ea = 1
 
+    # creates the starting snake
+    snake(postions,ea,scr)
+
+    while 1:
+        key = scr.getch()         
+        time.sleep(0.3) 
+        dire = checkkey(key,postions,ea,scr,dire)
+        mm = movement(dire)
+        
+        postions[1] = mm[0] + postions[1]
+        postions[0] = mm[1] + postions[0]
+        snake(postions,ea,scr)
+
+        # quit when q is hit
+        if postions[4] == 1:
+            break
+
+        # adds one to the string in snake when you reach the random point
+        if postions[1] == postions[3] and postions[0] == postions[2]:
+            ea +=1
+            postions[2] = random.randint(1,maxy)
+            postions[3] = random.randint(1,maxx)
+        
+        # ends game when you hit a border to avoid crashing 
+        if postions[1]  == dim[1]-1 or postions[0] == dim[0]-1 or postions[1] == 1 or postions[0] == 1:
+            break
+
+    term(scr)
+
+main(scr)
